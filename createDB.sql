cf72ce2966e5b491ec6f2d535615e8539a08f553
Vedansh Agarwal <hithisisvedansh@gmail.com>
2022-09-29 16:37:45 +0530

    major changes

--- createDB.sql
+++ createDB.sql
@@ -9,7 +9,7 @@ CREATE TABLE `tempdb`.`customers` (
   `mobileNo` VARCHAR(15) NOT NULL UNIQUE,
   `companyName` VARCHAR(200) NOT NULL,
   `designation` VARCHAR(200) NULL,
-  `companyEmailId` VARCHAR(100) NOT NULL,
+  `companyEmailId` VARCHAR(100) NOT NULL UNIQUE,
   `country` VARCHAR(100) NULL,
   `createdOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `updatedOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
@@ -25,17 +25,10 @@ CREATE TABLE `tempdb`.`customer_otp` (
   `email` VARCHAR(100) NOT NULL,
   `otp` VARCHAR(100) NOT NULL);
 
-DELIMITER $$
-USE `tempdb`$$
-CREATE PROCEDURE `delete_expired_OTPs`()
-BEGIN
-	DELETE FROM `customer_otp` WHERE CURRENT_TIMESTAMP() - `current_timestamp` > 1500;
-END$$
-DELIMITER ;
-
 CREATE TABLE `tempdb`.`questions` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `sectionName` VARCHAR(200) NOT NULL,
+  `subSectionName` VARCHAR(200) NULL,
   `questionDescription` TEXT NOT NULL,
   `choiceDetails` TEXT NOT NULL,
   `createdOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
@@ -45,7 +38,7 @@ CREATE TABLE `tempdb`.`questions` (
   `questionHelp` VARCHAR(500) NULL,
   PRIMARY KEY (`id`));
   
-CREATE TABLE `tempdb`.`survey_answer` (
+CREATE TABLE `tempdb`.`survey_answers` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `customerId` VARCHAR(100) NOT NULL,
   `surveyAnswers` TEXT NOT NULL,
@@ -53,3 +46,107 @@ CREATE TABLE `tempdb`.`survey_answer` (
   `surveyEndDate` DATETIME NULL,
   `surveyCompleteFlag` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`));
+
+DELIMITER $$
+USE `tempdb`$$
+CREATE PROCEDURE `delete_expired_OTPs`()
+BEGIN
+	DELETE FROM `customer_otp` 
+    WHERE CURRENT_TIMESTAMP() - `current_timestamp` > 1600;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `check_otp`(
+IN `email_input` VARCHAR(100), 
+IN `otp_input` VARCHAR(100), 
+IN `customer_id_input` VARCHAR(200), 
+IN `refresh_token_input` VARCHAR(300))
+BEGIN
+    SET @var1 = (SELECT `otp` FROM `customer_otp` WHERE `email` = `email_input` AND `otp` = `otp_input`);
+    SET @user_id = customer_id_input;
+    IF (NOT ISNULL(@var1)) THEN
+		SET @var2 = (SELECT `companyEmailId` FROM `customers`  WHERE `companyEmailId` = `email_input`);
+        IF(ISNULL(@var2)) THEN
+			INSERT INTO `customers` (`customerId`, `customerName`, `mobileNo`, `companyName`, `companyEmailId`, `refreshToken`) 
+			VALUE (`customer_id_input`, "", "", "", `email_input`, `refresh_token_input`);
+		ELSE
+			UPDATE `customers` SET `refreshToken` = `refresh_token_input` WHERE `companyEmailId` = `email_input`;
+			SET @user_id = (SELECT `customerId` FROM `customers` WHERE `companyEmailId` = `email_input`);
+        END IF;
+        SELECT @user_id AS `customer_id`;
+    END IF;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `insert_otp` (IN `email_input` VARCHAR(100), IN `otp_input` VARCHAR(100))
+BEGIN
+	INSERT INTO `customer_otp` (email, otp) VALUES (`email_input`, `otp_input`);
+    SELECT COUNT(*) AS `noOfLinkedAccounts` FROM `customers` WHERE `companyEmailId` = `email_input`;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `enter_customer_details` (
+IN `name_input` VARCHAR(200), 
+IN `mob_no_input` VARCHAR(15), 
+IN `company_name_input` VARCHAR(200), 
+IN `designation_input` VARCHAR(200), 
+IN `email_input` VARCHAR(100),
+IN `country_input` VARCHAR(100),
+IN `company_url_input` VARCHAR(200))
+BEGIN
+	SET @var1 = (SELECT `registrationStatus` FROM `customers` WHERE `companyEmailId` = `email_input`);
+    IF(@var1 = 0) THEN
+		UPDATE `customers` 
+        SET `customerName` = `name_input`, 
+			`companyName` = `company_name_input`, 
+			`mobileNo` = `mob_no_input`, 
+			`designation` = `designation_input`, 
+			`country` = `country_input`, 
+			`companyUrl` = `company_url_input`, 
+			`registrationStatus` = 1,
+            `createdOn` = CURRENT_TIMESTAMP(),
+            `updatedOn` = CURRENT_TIMESTAMP()
+		WHERE `companyEmailId` = `email_input`;
+        SELECT 0 AS `completionStatus`;
+	ELSE
+		SELECT -1 AS `completionStatus`;
+    END IF;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `update_customer_details` (
+IN `name_input` VARCHAR(200), 
+IN `mob_no_input` VARCHAR(15), 
+IN `company_name_input` VARCHAR(200), 
+IN `designation_input` VARCHAR(200), 
+IN `email_input` VARCHAR(100),
+IN `country_input` VARCHAR(100),
+IN `company_url_input` VARCHAR(200))
+BEGIN
+	UPDATE `customers` 
+        SET `customerName` = IFNULL(`name_input`, `customerName`), 
+			`companyName` = IFNULL(`company_name_input`, `companyName`), 
+			`mobileNo` = IFNULL(`mob_no_input`, `mobileNo`), 
+			`designation` = IFNULL(`designation_input`, `designation`), 
+			`country` = IFNULL(`country_input`, `country`), 
+			`companyUrl` = IFNULL(`company_url_input`, `companyUrl`), 
+            `updatedOn` = CURRENT_TIMESTAMP()
+		WHERE `companyEmailId` = `email_input`;
+        
+END$$
+DELIMITER ;
+
+CREATE VIEW `get_customer_details` AS
+SELECT 
+`tempdb`.`customers`.`customerName` AS `customerName`,
+`tempdb`.`customers`.`companyName` AS `companyName`,
+`tempdb`.`customers`.`companyUrl` AS `companyUrl`,
+`tempdb`.`customers`.`designation` AS `designation`,
+`tempdb`.`customers`.`companyEmailId` AS `companyEmailId`,
+`tempdb`.`customers`.`mobileNo` AS `mobileNo`,
+`tempdb`.`customers`.`country` AS `country`
+FROM `tempdb`.`customers`;
---
+++ server.js
@@ -0,0 +1,271 @@
+// .env configuration
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Database Connection
+const mysql = require("mysql2");
+const db = mysql.createConnection({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,  
+  database: process.env.DB_DATABASE,
+});
+
+// Express Configuration
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Nodemailer Configuration
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, 
+    auth: {
+      user: process.env.SENDER_GMAIL, 
+      pass: process.env.SENDER_APP_PASSWORD, 
+    }
+});
+
+// Middleware
+function checkAuth(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if(token == null) return res.status(statusCodes.accessDenied).json({message: "Access Denied"});
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+        var refreshToken = aes256.decrypt(process.env.REFRESH_ENCRYPTION_KEY, decoded.refreshToken);
+
+        db.query("SELECT COUNT(*) AS tokenExists FROM customers WHERE refreshToken = ?", [refreshToken],
+        (err, result) => {
+            if(err) {
+                console.log(err);
+                return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: err.message});
+            } else {
+                if(result.length == 0) {
+                    return res.status(statusCodes.accessDenied).json({message: "Access Denied"});
+                } else {
+                    req.body.email = decoded.email;
+                    req.body.customer_id = decoded.customer_id;
+                    next();
+                }
+            }
+        });
+    } catch(err) {
+        console.log(err);
+        return res.status(statusCodes.accessDenied).json({message: "Access Denied"});
+    }
+}
+
+function checkRefresh(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if(token == null) return res.status(statusCodes.accessDenied).json({message: "Access Denied"});
+
+    try{
+        jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+        var refreshToken = token;
+
+        db.query("SELECT companyEmailId, customerId FROM customers WHERE refreshToken = ?", [refreshToken],
+        (err, result) => {
+            if(err) {
+                console.log(err);
+                return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: err.message});
+            } else {
+                if(result.length == 0) {
+                    return res.status(statusCodes.accessDenied).json({message: "Access Denied"});
+                } else {
+                    req.body.refreshToken = refreshToken;
+                    req.body.email = result[0].companyEmailId;
+                    req.body.customer_id = result[0].customerId;
+                    next();
+                }
+            }
+        });
+    } catch(err) {
+        console.log(err);
+        return res.status(statusCodes.accessDenied).json({message: "Access Denied"});
+    }
+}
+
+
+// Imports for Routes
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+const aes256 = require("aes256");
+const {statusCodes} = require("./statusCodes");
+
+// Routes
+app.get("/edify/customer/generate-access-token", checkRefresh, (req, res) => {
+    const {refreshToken, email, customer_id} = req.body;
+    const encryptedRefresh =  aes256.encrypt(process.env.REFRESH_ENCRYPTION_KEY, refreshToken); 
+    const accessToken = jwt.sign({refreshToken: encryptedRefresh, customer_id: customer_id, email: email}, process.env.JWT_ACCESS_SECRET, {expiresIn: process.env.ACCESS_TOKEN_LIFE});
+    return res.status(statusCodes.success).json({message: "Access Token Generation Successful", accessToken: accessToken});
+});
+
+app.post("/edify/customer/enter-email", (req, res) => {
+    const {email} = req.body;
+
+    let otp = Math.floor(100000 + Math.random() * 900000);
+    db.query("CALL insert_otp(?, ?)", [email, otp],
+    async (err, result) => {
+        if(err) {
+            console.log(err);
+            return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: err.message});
+        } else {
+            if(!result || !result[0] || !result[0][0]) {
+                return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: "No enteries recieved from database."});
+            }
+            var subject, text;
+            var status, responseMessage;
+            if(result[0][0].noOfLinkedAccounts === 0) {
+                var mailParts = email.split("@");
+                var hashedEmail = mailParts[0].substring(0,2) + new Array(mailParts[0].length-3).join("*") + mailParts[0].substring(mailParts[0].length-2)+"@"+mailParts[1];
+                subject = "Survey Account Email verification security Code";
+                text = `Survey Code\n\nPlease use the following security code for verifying the company email address ${hashedEmail}.\n\n\n\n\nSecurity Code : ${otp}\n\nIf you didn't request this code, you can safely ignore this email. Someone else might have typed your email address by mistake.\n\nThanks,\nSurvey Team`;
+                status = statusCodes.resourceCreated;
+                responseMessage = "OTP for registration sent to your email";
+            } else {
+                subject = "Survey Account Login security Code";
+                text = `Survey Code\n\nPlease use the following security code for logging into the Survey Application.\n\n\n\n\nSecurity Code : ${otp}\n\nIf you didn't request this code, you can safely ignore this email. Someone else might have typed your email address by mistake.\n\nThanks,\nSurvey Team`;
+                status = statusCodes.success;
+                responseMessage = "OTP for login sent to your email";
+            }
+            try {
+                await transporter.sendMail({
+                    from: "Survey Team <" + process.env.SENDER_GMAIL + ">",
+                    to: email,
+                    subject: subject,
+                    text: text
+                });
+            }
+            catch(err) {
+                console.log(err);
+                return res.status(statusCodes.errorInSendingEmail).json({message: "Error in sending email."});
+            }
+            return res.status(status).json({message: responseMessage});
+        }
+    });
+});
+
+app.post("/edify/customer/verify-otp", (req, res) => {
+    const {email, otp} = req.body;
+    let customer_id = uuidv4();
+    const refreshToken = jwt.sign({email: email}, process.env.JWT_REFRESH_SECRET, {expiresIn: process.env.REFRESH_TOKEN_LIFE});
+    db.query("CALL check_otp(?, ?, ?, ?)", [email, otp, customer_id, refreshToken],
+    (err, result) => {
+        if(err) {
+            console.log(err);
+            return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: err.message});
+        } else {
+            if(result && result[0] && result[0].length > 0) {
+                customer_id = result[0][0].customer_id;
+                const encryptedRefresh =  aes256.encrypt(process.env.REFRESH_ENCRYPTION_KEY, refreshToken); 
+                const accessToken = jwt.sign({refreshToken: encryptedRefresh, customer_id: customer_id, email: email}, process.env.JWT_ACCESS_SECRET, {expiresIn: process.env.ACCESS_TOKEN_LIFE});
+                return res.status(statusCodes.success).json({message: "OTP verified", accessToken: accessToken, refreshToken: refreshToken});
+            } else {
+                return res.status(statusCodes.invalidCredentials).json({message: "Invalid OTP"});
+            }
+        }
+    });
+});
+
+app.get("/edify/customer/get-details", checkAuth, (req, res) => {
+    const {email} = req.body;
+
+    db.query("SELECT * FROM get_customer_details WHERE companyEmailId = ?", [email],
+    (err, result) => {
+        if(err) {
+            console.log(err);
+            return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: err.message});
+        } else {
+            if(result.length == 0) {
+                return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: "No such user found"});
+            } else {
+                return res.status(statusCodes.success).json({message: "User Details", data: result[0]});
+            }
+        }
+    });
+});
+
+app.post("/edify/customer/enter-details", checkAuth, (req, res) => {
+    var {customerName, companyName, mobileNo, email, designation, country, companyUrl} = req.body;
+    designation = designation || null;
+    country = country || null;
+    companyUrl = companyUrl || null;
+
+    db.query("CALL enter_customer_details(?, ?, ?, ?, ?, ?, ?)",
+    [customerName, mobileNo, companyName, designation, email, country, companyUrl],
+    (err, result) => {
+        if(err) {
+            console.log(err);
+            return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: err.message});
+        } else {
+            if(!result || !result[0] || !result[0][0]) {
+                return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: "No enteries recieved from database."});
+            } else {
+                if(result[0][0].completionStatus === -1) {
+                    return res.status(statusCodes.alreadyExists).json({message: "User Already Exists"});
+                } else {
+                    return res.status(statusCodes.success).json({message: "Details Registered"});
+                }
+            }
+        }
+    });
+});
+
+app.patch("/edify/customer/update-details", checkAuth, (req, res) => {
+    var {customerName, companyName, mobileNo, email, designation, country, companyUrl} = req.body;
+    customerName = customerName || null;
+    companyName = companyName || null;
+    mobileNo = mobileNo || null;
+    designation = designation || null;
+    country = country || null;
+    companyUrl = companyUrl || null;
+
+    db.query("CALL update_customer_details(?, ?, ?, ?, ?, ?, ?)",
+    [customerName, mobileNo, companyName, designation, email, country, companyUrl],
+    (err, result) => {
+        if(err) {
+            console.log(err);
+            return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: err.message});
+        } else {
+            return res.status(statusCodes.success).json({message: "Details Updated"});
+        }
+    });
+});
+
+app.post("/edify/user/add-question", (req, res) => {
+    var {sectionName, subSectionName, quesNo, questionDescription, choiceDetails, createdBy, questionHelp} = req.body;
+    questionDescription = JSON.stringify({quesNo: quesNo, questionDescription: questionDescription});
+    choiceDetails = JSON.stringify(choiceDetails);
+    questionHelp = questionHelp || null;
+    subSectionName = subSectionName || null;
+
+    db.query("INSERT INTO questions (sectionName, subSectionName, questionDescription, choiceDetails, createdBy, updatedBy, questionHelp) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    [sectionName, subSectionName, questionDescription, choiceDetails, createdBy, createdBy, questionHelp],
+    (err) => {
+        if(err) {
+            console.log(err);
+            return res.status(statusCodes.databaseError).json({message: "Database Error", errMsg: err.message});
+        } else {
+            return res.status(statusCodes.success).json({message: "Question Added"});
+        }
+    });
+});
+
+app.patch("/edify/user/update-question/:ques_id", (req, res) => {
+    // yet to finish
+});
+
+// Server Start
+app.listen(port, () => console.log(`Server listening on http://localhost:${port}/`));
+
+setInterval(() => {
+    db.query("CALL delete_expired_OTPs()");
+}, 1000);
